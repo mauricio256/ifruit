25b48243af5b5eaaefabe2ad3802e570bba2207c
Maurício Franca <mauricio96franca>
2023-01-27 23:54:46 -0300

    2 dia de trabalho

--- painel.php
+++ painel.php
@@ -1,5 +1,7 @@
 <?php
     session_start();
+    include("php/busca_ordem_servico.php");
+
     if( !isset($_SESSION['login']) ):
         header('Location:../php/index.html');
     endif;
@@ -20,9 +22,11 @@
     <body>
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <!-- Navbar Brand-->
-            <a class="navbar-brand ps-3" href="index.html">ifruit</a>
+            <a class="navbar-brand ps-3" href="painel.php">ifruit</a>
             <!-- Sidebar Toggle-->
-            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+            <?php if($_SESSION['login']['funcao'] == "fiscal"):?>
+                <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+            <?php endif; ?>
             <!-- Navbar Search-->
             <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
                 <div class="input-group">
@@ -35,15 +39,38 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#!">opcao</a></li>
-                        <li><a class="dropdown-item" href="#!">Meu perfil</a></li>
-                        <li><a class="dropdown-item" href="#!">opcao</a></li>
+                        <li><a class="dropdown-item " href="#!">Meu perfil</a></li>
+                        <li><a class="dropdown-item " href="#!">Opcao1</a></li>
+                        <li><a class="dropdown-item " href="#!">Opcao2</a></li>
                         <li><hr class="dropdown-divider" /></li>
-                        <li><a class="dropdown-item" href="php/processa_logout.php">Sair</a></li>
+                        <li><a class="dropdown-item text-danger" href="php/processa_logout.php">Sair</a></li>
                     </ul>
                 </li>
             </ul>
         </nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <?php if($_SESSION['login']['funcao'] == "fiscal"): /////////////// inicio da condicao de fiscal logado/////////////////////////?>
+
         <div id="layoutSidenav">
             <div id="layoutSidenav_nav">
                 <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
@@ -131,6 +158,7 @@
                                     <code>body</code>
                                     , the top navigation and side navigation will become static on scroll. Scroll down this page to see an example.
                                 </p>
+                                
                             </div>
                         </div>
                     </div>
@@ -139,20 +167,191 @@
                     <div class="container-fluid px-4">
                         <div class="d-flex align-items-center justify-content-between small">
                             <div class="text-muted">Copyright &copy; ifruit 2022</div>
-                            <div>
-                                <p?>Desenvolvido por:
-                                <a href="#">Friends &amp; Acadêmic</a></p>
-                            </div>
+                            <div class="text-muted">Desenvolvido por: <a href="">Friends Academic</a></div>
+                            
+                               
+                            
                         </div>
                     </div>
                 </footer>
             </div>
         </div>
+
+        <?php endif; //////////////////////////////////////////// fim da condicao?>
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+        <?php if($_SESSION['login']['funcao'] == "usuario"): /////////////// inicio da condicao de TELA DE FUNCIONARIO logado/////////////////////////?>
+           
+            <div class="alert-dark p-3 text-center" role="alert">   
+                <strong>Usuário: </strong><?php print $_SESSION['login']['nome']; print " ".$_SESSION['login']['sobrenome'] ?>
+            </div>
+
+            <a class="float-end m-3" href="painel.php"><img src="https://cdn.icon-icons.com/icons2/72/PNG/256/refresh_arrows_14418.png") width="50"></a>
+            <div class="container-xl p-lg-3 p-5 mt-5">
+                <div class="d-grid gap-2">
+
+                    <!-- Button trigger modal ATIVIDADES -->
+                    <button type="button" class="btn btn-info btn-lg" data-bs-toggle="modal" data-bs-target="#exampleModal1">
+                    <img src="img/atividades.png" alt="Imagem responsiva" width="50"><strong>ATIVIDADES </strong> 
+                    </button>
+
+                    <!-- Modal -->
+            
+                    <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-fullscreen-xxl-down" >
+                        <div class="modal-content">
+                        <div class="modal-header btn btn-info">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Atividades</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body row">
+                            
+                            <!-- Impressao dos valores do banco -->
+                            <?php foreach(ordem_servico_atividades($_SESSION['login']['cod_funcionario_pk']) as $row){ ?> 
+                                
+                                <div class="col-6">
+                                    <div class="card mb-3">
+                                        <p class="btn btn-warning">Lote: <?php print $row['cod_lote_fk']; ?> | Válvula: <?php print $row['cod_lote_fk']; ?></p>
+                                        <div class="card-body">
+                                            <p class="card-text"><strong>Fiscal:</strong><br> <?php print $row['fiscal']; ?></p>
+                                            <p class="card-text"><strong>Conteúdo:</strong><br> <?php print $row['conteudo']; ?></p>
+                                            <p class="card-text"><strong>Meta:</strong> <?php print $row['meta']; ?>
+                                            <p class="card-text"><strong>Concluidos:</strong> <?php print $row['colhida']; ?></p>
+                                        </div>
+                                    </div>
+                                </div> 
+
+                            <?php } ?>
+                            <!-- FIM da impressao dos valores do banco -->
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-info" data-bs-dismiss="modal">Fechar</button>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+
+
+                    <!-- /////////////////////////////////////////////////////////////////////////////////// -->
+
+                     <!-- Button trigger modal Pulvarizaçao -->
+                     <button type="button" class="btn btn-info btn-lg" data-bs-toggle="modal" data-bs-target="#exampleModal2">
+                    <img src="img/pulverizacao.png" alt="Imagem responsiva" width="50"><strong>PULVERIZAÇÃO</strong> 
+                    </button>
+
+                    <!-- Modal -->
+            
+                    <div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-fullscreen-xxl-down" >
+                        <div class="modal-content">
+                        <div class="modal-header btn btn-info">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Pulverização</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body row">
+                            
+                            <!-- Impressao dos valores do banco -->
+                            <?php foreach(ordem_servico_pulverizacao($_SESSION['login']['cod_funcionario_pk']) as $row){ ?> 
+                                
+                                <div class="col-6">
+                                    <div class="card mb-3">
+                                        <p class="btn btn-warning">Lote: <?php print $row['cod_lote_fk']; ?> | Válvula: <?php print $row['cod_lote_fk']; ?></p>
+                                        <div class="card-body">
+                                            <p class="card-text"><strong>Fiscal:</strong><br> <?php print $row['fiscal']; ?></p>
+                                            <p class="card-text"><strong>Conteúdo:</strong><br> <?php print $row['conteudo']; ?></p>
+                                            <p class="card-text"><strong>Meta:</strong> <?php print $row['meta']; ?>
+                                            <p class="card-text"><strong>Concluidos:</strong> <?php print $row['colhida']; ?></p>
+                                        </div>
+                                    </div>
+                                </div> 
+
+                            <?php } ?>
+                            <!-- FIM da impressao dos valores do banco -->
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-info" data-bs-dismiss="modal">Fechar</button>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+
+
+                    <!-- /////////////////////////////////////////////////////////////////////////////////// -->
+
+                     <!-- Button trigger modal adubacao -->
+                     <button type="button" class="btn btn-info btn-lg" data-bs-toggle="modal" data-bs-target="#exampleModal3">
+                    <img src="img/adubacao.png" alt="Imagem responsiva" width="50"><strong>ADUBAÇÃO</strong> 
+                    </button>
+
+                    <!-- Modal -->
+            
+                    <div class="modal fade" id="exampleModal3" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-fullscreen-xxl-down" >
+                        <div class="modal-content">
+                        <div class="modal-header btn btn-info">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Adubação</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body row">
+                            
+                            <!-- Impressao dos valores do banco -->
+                            <?php foreach(ordem_servico_adubacao($_SESSION['login']['cod_funcionario_pk']) as $row){ ?> 
+                                
+                                <div class="col-6">
+                                    <div class="card mb-3">
+                                        <p class="btn btn-warning">Lote: <?php print $row['cod_lote_fk']; ?> | Válvula: <?php print $row['cod_lote_fk']; ?></p>
+                                        <div class="card-body">
+                                            <p class="card-text"><strong>Fiscal:</strong><br> <?php print $row['fiscal']; ?></p>
+                                            <p class="card-text"><strong>Conteúdo:</strong><br> <?php print $row['conteudo']; ?></p>
+                                            <p class="card-text"><strong>Meta:</strong> <?php print $row['meta']; ?>
+                                            <p class="card-text"><strong>Concluidos:</strong> <?php print $row['colhida']; ?></p>
+                                        </div>
+                                    </div>
+                                </div> 
+
+                            <?php } ?>
+                            <!-- FIM da impressao dos valores do banco -->
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-info" data-bs-dismiss="modal">Fechar</button>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                    <!-- /////////////////////////////////////////////////////////////////////////////////// -->
+  
+
+                </div>
+            </div>
+
+        <?php endif; ?>
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="js/scripts.js"></script>
-        <script>
-
-
-        </script>
     </body>
 </html>
